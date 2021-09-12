fe7d8db092fab954b9de92272e719210b497e62c
Biola <52200708+decorouz@users.noreply.github.com>
2021-09-12 17:59:58 +0100

    Add database manager

---
+++ chapter06/database.py
@@ -0,0 +1,84 @@
+"""The database module provides most of what you need to manage book data, including the following:
+        1. Creating a table
+        Adding or deleting a record
+        Listing the records in a table
+        Selecting and sorting records from a table based on some criteria
+        Counting the number of records in a table
+    """
+import sqlite3
+
+
+class DatabaseManager:
+    # Database connection and the ability to execute arbitrary statments on the connection
+    def __init__(self, database_filename):
+        self.connection = sqlite3.connect(database_filename)
+
+    def _del__(self):
+        self.connection.close()
+
+    def _execute(self, statement, values=None):
+        with self.connection:
+            cursor = self.connection.cursor()
+            cursor.execute(statement, values or [])
+            return cursor
+# Creating Tables
+    # Determine the column names for the table
+    # Determine the data type of each column
+    # Constrcut the right SQL statement to create a table with those columns
+
+    def create_table(self, table_name, columns):
+        columns_with_types = [
+            f"{column_name} {data_type}" for column_name, data_type in columns.items()
+        ]
+        self._execute(
+            f"""
+            CREATE TABLE IF NOT EXISTS {table_name}
+            ({", ".join(columns_with_types)});
+            """
+        )
+# Adding Records
+
+    def add(self, table_name, data):
+        placeholders = ", ".join("?" * len(data))
+        column_names = ", ".join(data.keys())
+        column_values = tuple(data.value())
+
+        self._execute(
+            f"""
+            INSERT INTO {table_name}
+            ({column_names})
+            VALUES ({placeholders})
+            """,
+            column_values
+        )
+# Deleting Records
+
+    def delete(self, table_name, criteria):
+        placeholders = [f"{column}" for column in criteria.keys()]
+        delete_criteria = " AND ".join(placeholders)
+
+        self._execute(
+            f"""
+            DELETE FROM {table_name}
+            WHERE {delete_criteria}
+            """,
+            tuple(criteria.values())
+        )
+# Selecting and Sorting Records
+
+    def select(self, table_name, criteria=None, order_by=None):
+        criteria = criteria or {}
+
+        query = f"SELECT * FROM {table_name}"
+        if criteria:
+            placeholders = [f"{column} = ? " for column in criteria.keys()]
+            select_criteria = " AND ".join(placeholders)
+            query += f" WHERE {select_criteria}"
+
+        if order_by:
+            query += f" ORDER BY {order_by}"
+
+        return self._execute(
+            query,
+            tuple(criteria.values())
+        )
